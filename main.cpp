03752493ac1e6b5ab668580eb2261ab298e84676
Remdex <remdex@gmail.com>
2015-02-25 19:08:05 +0100

    Create settings file if it does not exists

--- main.cpp
+++ main.cpp
@@ -27,6 +27,16 @@ QString settingsFilename = qApp->applicationDirPath() + "/settings.xml";
 if(argc > 1)
 {
     settingsFilename = QString::fromUtf8(argv[1]);
+} else {
+    QFile file(settingsFilename);
+
+    /*Create settings file if it does not exists*/
+    if (!file.exists()) {
+       file.open(QIODevice::WriteOnly | QIODevice::Text);
+       QTextStream out(&file);
+       out << "<?xml version='1.0' encoding='utf-8'?><pmsettings><username></username><password></password><host></host><autologin>false</autologin><language>en</language><remember>true</remember><autooffline>0</autooffline><offlinetimeout>180</offlinetimeout></pmsettings>\n";
+       file.close();
+    }
 }
 
 qApp->addLibraryPath( qApp->applicationDirPath() + "/plugins");
@@ -42,8 +52,8 @@ delete pmsettings;
 app.installTranslator(&translator);
 
 //Inicijuojam DB
-QCoreApplication::setOrganizationName("Remdex");
-QCoreApplication::setOrganizationDomain("remdex.info");
+QCoreApplication::setOrganizationName("Live Helper Chat");
+QCoreApplication::setOrganizationDomain("livehelperchat.com");
 QCoreApplication::setApplicationName("Live helper chat");
 
 LoginDialog *lgnDialog = new LoginDialog(0,true,&settingsFilename);
